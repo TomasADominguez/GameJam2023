4d7479a6719f4fa20edd1ca0af4456e73766fc91
TomasADominguez <tomasagustindominguez8@gmail.com>
2023-02-04 19:24:01 -0300

    Added a new animation and fixed the transition between animations

--- Assets/_Game/_Player/Scripts/CharacterMovement.cs
+++ Assets/_Game/_Player/Scripts/CharacterMovement.cs
@@ -15,8 +15,6 @@ public class CharacterMovement : MonoBehaviour
 
     private Animator playerAni;
 
-    //private float carryingObject, startRunning;
-
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -25,14 +23,6 @@ public class CharacterMovement : MonoBehaviour
 
         characterController = new CharacterController();
         characterController.Player.Enable();
-
-        //carryingObject = 0f;
-        //startRunning = 0f;
-    }
-
-    private void Update()
-    {
-        //playerAni.SetFloat("CarryingObject", Mathf.Lerp(playerAni.GetFloat("CarryingObject"), carryingObject, .2f));
     }
 
     private void FixedUpdate()
@@ -45,26 +35,19 @@ public class CharacterMovement : MonoBehaviour
         Vector2 inputVector = characterController.Player.Movement.ReadValue<Vector2>();
         currentInputVector = Vector2.SmoothDamp(currentInputVector, inputVector, ref currentVelocity, inputSmoothSpeed);
 
+        playerAni.SetFloat("StartRunning", rb.velocity.magnitude);
+        playerAni.SetFloat("CarryingObject", rb.velocity.magnitude);
+
         if (isPlayerCarryingObject)
         {
             rb.AddForce(new Vector3(currentInputVector.x, currentInputVector.y, 0f) * (movementSpeed * carryingObjectModifier));
-            //carryingObject = 1f;
             playerAni.SetFloat("CarryingObject", 1f);
+            
         }
         else
         {
             rb.AddForce(new Vector3(currentInputVector.x, currentInputVector.y, 0f) * movementSpeed);
-            //carryingObject = 0f;
             playerAni.SetFloat("CarryingObject", 0f);
-        }
-
-        if(inputVector == Vector2.zero)
-        {
-            playerAni.SetFloat("StartRunning", 0f);
-        }
-        else
-        {
-            playerAni.SetFloat("StartRunning", 1f);
         }
     }
 }
